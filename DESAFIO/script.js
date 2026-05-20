c9d59aeed9bdcb0be57da1d7c830b85c0a3825cf
I1HSD <arthursudario.10@gmail.com>
2026-05-20 09:57:08 -0300

    array alunos

---
+++ DESAFIO/script.js
@@ -0,0 +1,9 @@
+const alunos = [{name: "Gustavo", idade: "16", disciplinaPreferida: "filosofia"},
+                {name: "Mayara", idade: "16", disciplinaPreferida: "matematica"},
+                {name: "Livia", idade: "16", disciplinaPreferida: "geografia"},
+                {name: "Anna", idade: "16", disciplinaPreferida: "portugues"},
+                {name: "Arthur", idade: "17", disciplinaPreferida: "educação física"}];
+
+console.log(alunos[0].name);
+
+alert("O nome desse aluno é: " + alunos[0].name);
